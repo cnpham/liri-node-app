09b5b9f2aeac2696c5c114594ce1e8b690f48b2a
cnpham <44155537+cnpham@users.noreply.github.com>
2019-04-18 02:42:15 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 
 The LIRI Bot app utilizes the Spotify API in order to execute specific commands which will
 access the Spotify database of songs, movies, etc.
+
+# How to use
+
+node liri.js concert-this <band name>
+
+node liri.js spotify-this-song <song name>
+
+node liri.js movie-this <movie name>
--- liri.js
+++ liri.js
@@ -1,39 +1,87 @@
 require("dotenv").config();
 
+var Spotify = require('node-spotify-api');
+var spotify = new Spotify({
+    id: process.env.SPOTIFY_ID,
+    secret: process.env.SPOTIFY_SECRET
+});
+
 var fs = require("fs");
 
 var keys = require("./keys.js");
 
 // npm package inclusions
 var spotify = new Spotify(keys.spotify);
+//var omdb = new Omdb(keys.omdb);
 var axios = require("axios");
 
-var inputString = spotify.process.argv;
+// take command line args for a command into an array
+var inputString = process.argv;
 
 var command = inputString[2];
 var input = inputString[3];
 
 if (command === "concert-this") {
-    console.log("https://rest.bandsintown.com/artists/" + input + "/events?app_id=codingbootcamp");    
+    var bands = "https://rest.bandsintown.com/artists/" + input + "/events?app_id=codingbootcamp";
+    axios.get(bands).then(
+        function (response) {
+            console.log("* " + response.data.Name + ".");
+            console.log("* " + respoonse.data.Location + ".");
+            console.log("* " + response.data.Date + ".");
+        }
+    );
 }
 
 else if (command === "spotify-this-song") {
-    output = parseFloat(num1) - parseFloat(num2);
+    var getArtistNames = function (artist) {
+        return artist.name;
+    }
+    if (typeof input === "undefined") {
+        input = "The Sign";
+    }
+    spotify.search({
+        type: "track",
+        query: input,
+    },
+        function (err, data) {
+            if (err) {
+                return console.log('Error occurred: ' + err);
+            }
+
+            var songs = data.tracks.items;
+
+            for (var i = 0; i < songs.length; i++) {
+                console.log(i)
+                console.log("artist(s): " + songs[i].artists.map(getArtistNames));
+                console.log("song name: " + songs[i].name);
+                console.log("preview song: " + songs[i].preview_url);
+                console.log("album: " + songs[i].album.name);
+            }
+        }
+    );
 }
 
 else if (command === "movie-this") {
-    console.log("* " + title + ".");
-    console.log("* " + year + ".");
-    console.log("* " + imdbRating + ".");
-    console.log("* " + rottenRating + ".");
-    console.log("* " + country + ".");
-    console.log("* " + language + ".");
-    console.log("* " + plot + ".");
-    console.log("* " + actors + ".");
+    if (typeof input === "undefined") {
+        input = "mr.nobody";
+    }
+    var omdbUrl = "http://www.omdbapi.com/?t=" + input + "&y=&plot=short&apikey=trilogy";
+    axios.get(omdbUrl).then(
+        function (response) {
+            console.log("* " + response.data.Title + ".");
+            console.log("* " + response.data.Year + ".");
+            console.log("* " + response.data.imdbRating + ".");
+            console.log("* " + response.data.tomatoRotten + ".");
+            console.log("* " + response.data.Country + ".");
+            console.log("* " + response.data.Language + ".");
+            console.log("* " + response.data.Plot + ".");
+            console.log("* " + response.data.Actors + ".");
+        }
+    );
 }
 
 else if (command === "do-what-it-says") {
-    fs.readFile("random.txt", "utf8", function(error, data) {
+    fs.readFile("random.txt", "utf8", function (error, data) {
         if (error) {
             return console.log(error);
         }
